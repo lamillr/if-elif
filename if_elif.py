5cd43d0c7a9359d0de46b4a3134577d9fc46b650
Lisa M <lamiller2200@gmail.com>
2019-03-27 15:01:09 -0400

    first commit

---
+++ if_elif.py
@@ -0,0 +1,46 @@
+print("Question 10")
+
+def traffic_report(light):
+    if light == 'red':
+        return 'stop'
+    elif light == 'yellow':
+        return 'slow'
+    elif light == 'green':
+        return 'go'
+print(traffic_report('yellow'))
+
+def fruit_color(fruit):
+    if fruit == 'apple':
+        return 'red'
+    elif fruit == 'banana':
+        return 'yellow'
+    elif fruit == 'pear':
+        return 'green'
+
+print(fruit_color('peach'))
+
+print("Question 12")
+
+def grade_report(grade):
+    if grade >= 80:
+        return 'excellent'
+    elif grade >= 50:
+        return 'pass'
+print(grade_report(50))
+print(grade_report(70))
+print(grade_report(40))
+print(grade_report(80))
+
+# For python visualizer
+def greeting(name):
+    return 'Hi, ' + name
+
+def exclaim(statement):
+    return statement + '!'
+
+def enthusiastic_greeting(name):
+    greeting_message=exclaim(greeting(name))
+    print(greeting_message)
+
+enthusiastic_greeting('Orion')
+
